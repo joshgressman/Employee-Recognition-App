e5674cdd4bc4dd1876e0852c59664488ed613b72
joshgressman <joshgressman@gmail.com>
2017-07-25 10:57:34 -0500

    added bootstrap and db connection

--- server.js
+++ server.js
@@ -7,11 +7,47 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 //API ROUTES
 var api = require('./server/routes/api');
 
 var app = express();
+
+//Mongo DB Connectoin
+//mongodb
+// mongoose.connect('mongodb://localhost/take12');
+// process.env.MONGODB_URI will only be defined if you
+// are running on Heroku
+if(process.env.MONGODB_URI != undefined) {
+    // use the string value of the environment variable
+    var dbLocation = 'mlab';
+    var uri = process.env.MONGODB_URI;
+    mongoose.Promise = global.Promise;
+
+    var promise = mongoose.connect(uri, {
+    useMongoClient: true,
+  /* other options */
+});
+
+} else {
+    // use the local database server
+    var dbLocation = 'local';
+    console.log('local DB')
+    mongoose.Promise = global.Promise;
+    var uri = 'mongodb://localhost:27017/employee_rewards';
+    var promise = mongoose.connect(uri, {
+    useMongoClient: true,
+    });
+}
+promise.then(function(db) {
+console.log('db connected to employee_rewards');
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('openUri', function() {
+  console.log('Connected to emloyee-rewards', dbLocation);
+});
+
+});
 
 //Parsers for post data
 app.use(bodyParser.json());
