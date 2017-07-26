011361f2f6b6a4899e516dbb852368a725bf9e43
joshgressman <joshgressman@gmail.com>
2017-07-26 14:54:17 -0500

    added employee schema and model

---
+++ schema/employee.js
@@ -0,0 +1,15 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+  firstName: {type: String, required: true, lowercase: true},
+  lastName: {type: String, required: true, lowercase: true},
+  company: {type: String, required: true, lowercase: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  points: Number,
+  awards: {type: Array, "default": []}
+});
+
+module.exports = mongoose.model('Employee', schema);
